917989c1aa2501df0c613b0e3bd8a72b20815b8c
Aceyoi <146310875+Aceyoi@users.noreply.github.com>
2025-03-03 12:27:22 +0900

    Add files via upload

--- Modul.py
+++ Modul.py
@@ -3,6 +3,27 @@ import numpy as np
 import torch
 
 def testcheck():
+    testlist = [[1, 2, 3], [4, 5, 6], [7,8,9]]
+    testmat = np.array([[1, 2, 3], [4, 5, 6], [7,8,9]])
+    testgpu = torch.tensor([[1., 2., 3.], [4., 5., 6.], [7.,8.,9.]], dtype=torch.long) # matmul не работает с целыми значениями smiledog
+    testcpu = torch.tensor([[1., 2., 3.], [4., 5., 6.], [7.,8.,9.]], device='cuda')
+
+    testlist1 = [[30, 36, 42], [66, 81, 96], [102,126,150]]
+    testmat1 = np.array([[30, 36, 42], [66, 81, 96], [102,126,150]])
+    testgpu1 = torch.tensor([[30., 36., 42.], [66., 81., 96.], [102., 126., 150.]], dtype=torch.long)
+    testcpu1 = torch.tensor([[30., 36., 42.], [66., 81., 96.], [102., 126., 150.]], device='cuda')
+
+    testlist2 = multiply_python(testlist, testlist)
+    testmat2 = np.dot(testmat, testmat)
+    testgpu2 = torch.matmul(testgpu, testgpu)
+    testcpu2 = torch.matmul(testcpu, testcpu)
+
+
+    assert(testlist2 == testlist1)
+    assert(np.array_equal(testmat1, testmat2))
+    assert(torch.equal(testgpu1, testgpu2))
+    assert(torch.equal(testcpu1, testcpu2))
+
     pass
 
 
@@ -40,6 +61,5 @@ def timechecktrchcpu(matrixtrchcpu1, matrixtrchcpu2):
 def timechecktrchgpu(matrixtrchgpu1, matrixtrchgpu2):
     start_time = time.time()
     torch.matmul(matrixtrchgpu1, matrixtrchgpu2)
-    torch.cuda.synchronize()  # Синхронизация для точного измерения времени на GPU
     end_time = time.time()
     return end_time - start_time
